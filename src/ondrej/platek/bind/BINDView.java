fe8d50a14b41e3e52f37598ebbcfb3cef17a9f0f
Ondrej Platek <ondrej.platek at seznam dot cz>
2011-11-13 17:14:38 +0100

    before adding source according to http://www.learnopengles.com/android-lesson-one-getting-started/

--- src/ondrej/platek/bind/BINDView.java
+++ src/ondrej/platek/bind/BINDView.java
@@ -1,35 +1,4 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ondrej.platek.bind;
-/*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
 import javax.microedition.khronos.egl.EGL10;
@@ -250,7 +219,7 @@ class BINDView extends GLSurfaceView {
 
         private void printConfig(EGL10 egl, EGLDisplay display,
                 EGLConfig config) {
-            int[] attributes = {
+            int[] attributes = {            		
                     EGL10.EGL_BUFFER_SIZE,
                     EGL10.EGL_ALPHA_SIZE,
                     EGL10.EGL_BLUE_SIZE,
