a8faaf88c98478066ee17007175b620f80ac76e1
Ondrej Platek <ondrej.platek at seznam dot cz>
2011-11-30 16:20:11 +0100

    got stuck of loading c->mvpMatrix ... probably not assigned to AppCtx c

--- trunk/jni/natRenderer.cpp
+++ trunk/jni/natRenderer.cpp
@@ -99,7 +99,6 @@ void checkGlError(const char* op) {
     }
 }
 
-/*
 ///////////////////// openGL matrix function ///////////////
 void esTranslate(ESMatrix *result, GLfloat tx, GLfloat ty, GLfloat tz)
 {
@@ -236,12 +235,13 @@ void esPerspective(ESMatrix *result, float fovy, float aspect, float nearZ, floa
 
    esFrustum( result, -frustumW, frustumW, -frustumH, frustumH, nearZ, farZ );
 }
-*/
+
 #define VERTEX_POS_INDX 0
 #define VERTEX_POS_SIZE 3
 
 #define INDEX_A_POSITION 	0
 #define INDEX_A_COLOR 		1
+#define INDEX_U_MVP 		0
 
 void loadAttributes(AppCtx * c);
 
@@ -296,16 +296,14 @@ struct AppCtx {
 
 
 
-//#define INDEX_U_MVP 		0
-//    "uniform mat4 u_mvpMatrix;                   \n"
-//	"   gl_Position = u_mvpMatrix * a_position;  \n"
-static const char gVertexShader[] = 
+static const char gVertexShader[] =
+    "uniform mat4 u_mvpMatrix;                  \n"
     "attribute vec4 a_position;					\n"
     "attribute vec4 a_color;					\n"
     "varying vec4   v_color;					\n"
     "void main() {						        \n"
 	"  v_color = a_color;				        \n"
-	"   gl_Position = a_position;  				\n"
+	"	gl_Position = u_mvpMatrix * a_position; \n"
     "}								            \n";
 
 static const char gFragmentShader[] = 
@@ -322,8 +320,8 @@ void renderFrame(AppCtx * c) {
     glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     checkGlError("glClear");
 
-//    glUniformMatrix4fv(INDEX_U_MVP , 1, GL_FALSE, (GLfloat*) c->mvpMatrix.m[0][0] );
-//	checkGlError("glUniformMatrix4fv");
+    glUniformMatrix4fv(INDEX_U_MVP , 1, GL_FALSE, (GLfloat*) c->mvpMatrix->m);
+	checkGlError("glUniformMatrix4fv");
 
     for(int i=0; i < c->parts_number; ++i) {
         glDrawElements(GL_TRIANGLES, c->parts_sizes[i], GL_UNSIGNED_BYTE, c->faces[i]);
@@ -361,11 +359,10 @@ bool setupGraphics(AppCtx * c) {
 //    glEnable(GL_CULL_FACE);
 //    checkGlError("glEnable(GL_CULL_FACE)");
 
-    LOGI("setupGraphics(%d, %d)", c->width, c->height);
-    GLuint gProgram = createProgram(gVertexShader, gFragmentShader);
-    glUseProgram(gProgram);
+    c->glProgram = createProgram(gVertexShader, gFragmentShader);
+    glUseProgram(c->glProgram);
     checkGlError("glUseProgram");
-    if (!gProgram) {
+    if (!c->glProgram) {
         LOGE("Could not create program.");
         return false;
     }
@@ -376,27 +373,29 @@ bool setupGraphics(AppCtx * c) {
     checkGlError("glViewport");
 
     float aspect = (GLfloat) c->width / c->height;
-//    ESMatrix perspective;
-//    ESMatrix modelView;
-//    esMatrixLoadIdentity(&perspective);
-//    esPerspective(&perspective, 40.0f, aspect,1.0f, 200.0f);
-//    esMatrixLoadIdentity(&modelView);
-//    esTranslate(&modelView, 0.0f, 0.0f, -20.0f);
-//    esMatrixMultiply(c->mvpMatrix, &modelView, &perspective);
+    ESMatrix perspective;
+    ESMatrix modelView;
+    esMatrixLoadIdentity(&perspective);
+    esPerspective(&perspective, 40.0f, aspect,1.0f, 200.0f);
+    esMatrixLoadIdentity(&modelView);
+    esTranslate(&modelView, 0.0f, 0.0f, -20.0f);
+    esMatrixMultiply(c->mvpMatrix, &modelView, &perspective);
     checkGlError("Matrix setup");
 
+    LOGI("setupGraphics(%d, %d)", c->width, c->height);
     return true;
 }
 
 /////// loadAttributes
 void loadAttributes(AppCtx * c) {
-//    glBindAttribLocation(c->glProgram, INDEX_U_MVP,"u_mvpMatrix");
-//    checkGlError("glBindAttribLocation .. u_mvpMatrix");
+    glBindAttribLocation(c->glProgram, INDEX_U_MVP,"u_mvpMatrix");
+    checkGlError("glBindAttribLocation .. u_mvpMatrix");
     glBindAttribLocation(c->glProgram, INDEX_A_POSITION, "a_position");
     checkGlError("glBindAttribLocation .. a_position");
     glBindAttribLocation(c->glProgram, INDEX_A_COLOR, "a_color");
     checkGlError("glBindAttribLocation .. a_color");
 
+    LOGI("loadAt 1");
     // TODO reinitialize the colors
 //    glVertexAttribPointer(INDEX_A_COLOR, 4, GL_FLOAT, GL_FALSE, sizeof(SVertex)+offset TODO, c->vertices);
 
@@ -404,24 +403,29 @@ void loadAttributes(AppCtx * c) {
     glVertexAttrib4fv(INDEX_A_COLOR,red);
     checkGlError("glVertexAttrib4fv");
 
+    LOGI("loadAt 2");
+    todo not loading mvpMatrix
 //    glUniformMatrix4fv(INDEX_U_MVP , 1, GL_FALSE, (GLfloat*) c->mvpMatrix.m[0][0] );
-//    checkGlError("glUniformMatrix4fv .. mvp");
+    glUniformMatrix4fv(INDEX_U_MVP , 1, GL_FALSE, (GLfloat*) c->mvpMatrix->m);
+    checkGlError("glUniformMatrix4fv .. mvp");
 
+    LOGI("loadAt 3");
 
     glVertexAttribPointer(INDEX_A_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(SVertex), c->vertices);
     checkGlError("glVertexAttribPointer");
     glEnableVertexAttribArray(INDEX_A_POSITION);
     checkGlError("glEnableVertexAttribArray");
-
+    LOGI("loadAt 4");
+    LOGI("setupGraphics(%d, %d)", c->width, c->height);
 }
 
 void zoom(AppCtx * c, float z) {
-//    esTranslate(c->mvpMatrix, 0.0, 0.0, -z);
+    esTranslate(c->mvpMatrix, 0.0, 0.0, -z);
 }
 
 void rotateAnchor(AppCtx * c, float dx, float dy) {
-//    esRotate(c->mvpMatrix, dx, 1.0, 0.0, 0.0);
-//    esRotate(c->mvpMatrix, dy, 0.0, 0.1, 0.0);
+    esRotate(c->mvpMatrix, dx, 1.0, 0.0, 0.0);
+    esRotate(c->mvpMatrix, dy, 0.0, 0.1, 0.0);
 }
 
 //////////////////// not really opengl functions ///////////
@@ -487,7 +491,6 @@ jobject objForArray(JNIEnv * env, jobject mythis, const char * memberName,const 
 void returnInt(JNIEnv * env, jobject mythis,const char * memberName,int v);
 
 JNIEXPORT void JNICALL Java_ondrej_platek_bind_NativeRenderer_init(JNIEnv * env, jobject mythis,jobjectArray Normals, jobjectArray Faces)  {
-    LOGI("start of all program init: %d");
 
     const char * str ="pAppCtx";
     int ic = extractInt(env, mythis,"pAppCtx");
@@ -501,32 +504,22 @@ JNIEXPORT void JNICALL Java_ondrej_platek_bind_NativeRenderer_init(JNIEnv * env,
     	releaseResources(c);
     }
 
-    LOGI("init 1");
-
     int raw_size = extractInt(env, mythis, "vertices_size");
     c->height = extractInt(env, mythis, "height");
     c->width = extractInt(env, mythis, "width");
     c->parts_number = extractInt(env, mythis, "parts_number");
 
-    LOGI("init 2");
     // jArrays -> I have to release them afterwards
     jobject mvdata = objForArray(env, mythis, "vertices", "[F");
     jfloatArray * arr = reinterpret_cast<jfloatArray*>(&mvdata);
     float * raw_vertices = env->GetFloatArrayElements(*arr, NULL);
 
-    LOGI("init 3");
-
     mvdata = objForArray(env, mythis, "parts_sizes", "[I");
     jintArray * arr2 = reinterpret_cast<jintArray*>(&mvdata);
     int * raw_parts_sizes = env->GetIntArrayElements(*arr2, NULL);
 
-    LOGI("init 4");
-
-    LOGI("before vertices init");
     c->numVertices = raw_size/3;
     c->vertices = new SVertex[c->numVertices]; // important to release before it
-
-    LOGI("init 5");
 
     int t;
     for(int i=0; i < c->numVertices; ++i) {
@@ -534,8 +527,6 @@ JNIEXPORT void JNICALL Java_ondrej_platek_bind_NativeRenderer_init(JNIEnv * env,
         c->vertices[i] = SVertex(raw_vertices[t], raw_vertices[t+1], raw_vertices[t+2]);
         c->vertices[i].LOG(i);
     }
-
-    LOGI("before parts_sizes");
 
     c->parts_sizes = new int[c->parts_number];
     for(int i = 0; i < c->parts_number; i++) {
@@ -586,8 +577,8 @@ JNIEXPORT void JNICALL Java_ondrej_platek_bind_NativeRenderer_step(JNIEnv * env,
       LOGE("NativeRender_step context is NULL");
     }
     else {
-    renderTestFrame(c);
-//    renderFrame(c);
+//    renderTestFrame(c);
+    renderFrame(c);
     }
 }
   
