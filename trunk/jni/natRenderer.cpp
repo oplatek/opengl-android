b289fd5d6ec9201e48d285a27cc961918db6a3dc
Ondrej Platek <ondrej.platek at seznam dot cz>
2012-01-02 23:55:00 +0100

    structs moved away

--- trunk/jni/natRenderer.cpp
+++ trunk/jni/natRenderer.cpp
@@ -271,6 +271,16 @@ void releaseVertexRes(AppCtx * c) {
     }
 }
 
+// should be only for debugging
+SVertex* Mat_x_Vertex(ESMatrix * esm, SVertex * v) {
+	SVertex tmp;
+	tmp.x = (esm->m[0][0] * v->x) +(esm->m[0][1] * v->y) +(esm->m[0][2] * v->z) +(esm->m[0][3] * 1) ;
+	tmp.y = (esm->m[1][0] * v->x) +(esm->m[1][1] * v->y) +(esm->m[1][2] * v->z) +(esm->m[1][3] * 1) ;
+	tmp.z = (esm->m[2][0] * v->x) +(esm->m[2][1] * v->y) +(esm->m[2][2] * v->z) +(esm->m[2][3] * 1) ;
+	memcpy(v,&tmp,sizeof(tmp));
+	return v;
+}
+
 
 /*
 mat4 LookAt(const vec3& eye, const vec3& target, const vec3& up)
--- trunk/jni/natRenderer.h
+++ trunk/jni/natRenderer.h
@@ -13,66 +13,8 @@
 
 #include "def.h"
 #include "esUtils.h"
+#include "natRenderer_general.h"
 
-
-struct SVertex {
-    GLfloat x,y,z,w;
-    GLfloat r,g,b;
-//    GLfloat t0_s,t0_t; // 1. texture coordinates
-//    GLfloat t1_s,t1_t; // 2. texture coordinates
-    SVertex() {
-              x = y = z = w = 0.0f; r = g = b = 0.5f; // grey color
-    }
-    SVertex(GLfloat x_, GLfloat y_, GLfloat z_, GLfloat w_ = 1.0f,
-            GLfloat r_ = 0.5f, GLfloat g_ = 0.5f, GLfloat b_ = 0.5f) {
-	  x = x_; y = y_; z = z_; w = w_; r = r_; g = g_; b = b_;
-    }
-    void LOG(int index) {
-      LOGI("v[%d].xyz=(%f, %f, %f, %f)",index,x,y,z,w);
-      LOGI("v[%d].rgb=(%f, %f, %f)",index,r,g,b);
-    }
-};
-
-struct Normal{
-    GLfloat x,y,z;
-    Normal() {
-    	x = y = z = 0.0f;
-    }
-    Normal(GLfloat x_, GLfloat y_, GLfloat z_) {
-       x = x_; y = y_; z = z_;
-    }
-};
-
-struct AppCtx {
-    int numVertices; // sum(part_sizes) == numVertices
-    int parts_number;
-    int width;
-    int height;
-    int * parts_sizes;
-    int glProgram;
-    Normal ** normals; // first dim[pointer]: part_index, second dim[GLfloat]: normals per part
-    GLubyte **  faces; //firrst dim[pointer]: part_index, second dim[GLubyte]: indeces to vertices per part
-    SVertex * vertices;
-    GLuint shaderIdx_a_position;
-    GLuint shaderIdx_a_color;
-    GLuint shaderIdx_u_mvpMatrix;
-    ESMatrix T; // translate matrix
-    ESMatrix S; // scale matrix
-    ESMatrix R; // rotation matrix
-    ESMatrix mvpMatrix;
-    AppCtx();
-    ~AppCtx();
-};
-
-//todo move
-SVertex* Mat_x_Vertex(ESMatrix * esm, SVertex * v) {
-	SVertex tmp;
-	tmp.x = (esm->m[0][0] * v->x) +(esm->m[0][1] * v->y) +(esm->m[0][2] * v->z) +(esm->m[0][3] * 1) ;
-	tmp.y = (esm->m[1][0] * v->x) +(esm->m[1][1] * v->y) +(esm->m[1][2] * v->z) +(esm->m[1][3] * 1) ;
-	tmp.z = (esm->m[2][0] * v->x) +(esm->m[2][1] * v->y) +(esm->m[2][2] * v->z) +(esm->m[2][3] * 1) ;
-	memcpy(v,&tmp,sizeof(tmp));
-	return v;
-}
 
 /////// Functions ////////
 
@@ -84,7 +26,8 @@ void zoom(AppCtx * c, float z);
 void rotateAnchor(AppCtx * c, float dx, float dy);
 bool setupGraphics(AppCtx * c);
 void modelViewBoundaries(SVertex * verArr, int sizeArr, GLfloat * rxmin, GLfloat * rxmax, GLfloat  * rymin, GLfloat * rymax, GLfloat  * rzmin, GLfloat * rzmax);
-void LOGm(ESMatrix *esm, AppCtx *c) ;
+void LOGm(ESMatrix *esm, AppCtx *c);
+SVertex* Mat_x_Vertex(ESMatrix * esm, SVertex * v);
 #ifdef __cplusplus
 extern "C" {
 #endif
