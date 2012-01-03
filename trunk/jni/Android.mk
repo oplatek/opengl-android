29df434c9e0cb58a346767379fbce324d89f516a
Ondrej Platek <ondrej.platek at seznam dot cz>
2012-01-03 11:48:28 +0100

    extracted jni to def.h and defined function for Linux, BUT CONDITIONAL COMPILATION DOES NOT WORK

--- trunk/jni/Android.mk
+++ trunk/jni/Android.mk
@@ -12,7 +12,7 @@ LOCAL_MODULE    := test
 LOCAL_CFLAGS    := -Werror
 LOCAL_SRC_FILES := natRenderer.cpp logFunctions.cpp
 LOCAL_LDLIBS    := -llog -lGLESv2
-LOCAL_STATIC_LIBRARIES := esUtils 
+LOCAL_STATIC_LIBRARIES := esUtils
 
 include $(BUILD_SHARED_LIBRARY)
 
--- trunk/jni/def.h
+++ trunk/jni/def.h
@@ -1,8 +1,31 @@
 #ifndef _Included_ondrej_platek_bind_def_
 #define _Included_ondrej_platek_bind_def_
 
-#define LOG_TAG "natRenderer"
-#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+//#define ANDROID // alternative #define LINUX
+//
+//#ifdef ANDROID
+    #include <android/log.h>
+    #define LOG_TAG "natRenderer"
+    #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+    #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+//#else
+//#ifdef LINUX
+//    #include <stdio.h> 
+//    #include <stdarg.h>
+//    void LOGI(const char * format, ...) {
+//        va_list argptr;
+//        va_start(argptr, format);
+//        vfprintf(stdout, format, argptr);
+//        va_end(argptr);
+//    }
+//
+//    void LOGE(const char* format, ...) {
+//        va_list argptr;
+//        va_start(argptr, format);
+//        vfprintf(stderr, format, argptr);
+//        va_end(argptr);
+//    }
+//#endif
+//#endif
 
 #endif
