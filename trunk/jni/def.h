2d1d316286fb65158b420163465d4f9ec58e536d
ondrej platek <ondrej dot platek at seznam dot cz>
2012-01-04 13:51:57 +0100

    set up conditional compilation from Makefile

--- trunk/jni/def.h
+++ trunk/jni/def.h
@@ -1,21 +1,17 @@
 #ifndef _Included_ondrej_platek_bind_def_
 #define _Included_ondrej_platek_bind_def_
 
-#define OS_ANDROID // alternative #define OS_LINUX
-//#define OS_LINUX // alternative #define OS_ANDROID
 
-#ifdef OS_ANDROID
-    #include <android/log.h>
-    #define LOG_TAG "natRenderer"
-    #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-    #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-#else
 #ifdef OS_LINUX
     #include <stdio.h> 
     #include <stdarg.h>
     void LOGI(const char * format, ...) ;
     void LOGE(const char* format, ...) ;
-#endif
+#else // should be default option since Android.mk is not set up with conditional comp
+    #include <android/log.h>
+    #define LOG_TAG "natRenderer"
+    #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+    #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 #endif
 
 #endif
--- trunk/jni/logFunctions.cpp
+++ trunk/jni/logFunctions.cpp
@@ -37,9 +37,11 @@ void LOGm(ESMatrix *esm, AppCtx *c) {
 // should be only for debugging it is slow in CPU
 SVertex* Mat_x_Vertex(ESMatrix * esm, SVertex * v) {
 	SVertex tmp;
-	tmp.x = (esm->m[0][0] * v->x) +(esm->m[0][1] * v->y) +(esm->m[0][2] * v->z) +(esm->m[0][3] * 1) ;
-	tmp.y = (esm->m[1][0] * v->x) +(esm->m[1][1] * v->y) +(esm->m[1][2] * v->z) +(esm->m[1][3] * 1) ;
-	tmp.z = (esm->m[2][0] * v->x) +(esm->m[2][1] * v->y) +(esm->m[2][2] * v->z) +(esm->m[2][3] * 1) ;
-	memcpy(v,&tmp,sizeof(tmp));
+	tmp.x = (esm->m[0][0] * v->x) +(esm->m[0][1] * v->y) +(esm->m[0][2] * v->z) +(esm->m[0][3] * v->w) ;
+	tmp.y = (esm->m[1][0] * v->x) +(esm->m[1][1] * v->y) +(esm->m[1][2] * v->z) +(esm->m[1][3] * v->w) ;
+	tmp.z = (esm->m[2][0] * v->x) +(esm->m[2][1] * v->y) +(esm->m[2][2] * v->z) +(esm->m[2][3] * v->w) ;
+	tmp.w = (esm->m[3][0] * v->x) +(esm->m[3][1] * v->y) +(esm->m[3][2] * v->z) +(esm->m[3][3] * v->w) ;
+    *v = tmp;
+//	memcpy(v,&tmp,sizeof(tmp));
 	return v;
 }
