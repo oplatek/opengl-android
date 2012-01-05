674c26c0c2487fe369bcc05e540bfe8ec0670bfc
ondrej platek <ondrej dot platek at seznam dot cz>
2012-01-05 04:03:29 +0100

    default zooming done. TODO save values for translation before and after rotation

--- trunk/jni/natRenderer.cpp
+++ trunk/jni/natRenderer.cpp
@@ -14,6 +14,12 @@
 #define INDEX_A_COLOR 		1
 #define INDEX_U_MVP 		0
 
+#define Z_NEAR              1.0f
+#define Z_FAR               100.0f
+#define ANGLE               45.0f
+#define TANGENS             0.414f 
+#define FRUS_COEF           0.2f
+
 //// DO NOT FORGET UPDATE ATTRIBUTES AT ////
 void bindShaderAttr(AppCtx *c) {
     c->shaderIdx_a_position=  glGetAttribLocation(c->glProgram, "a_position");
@@ -25,7 +31,7 @@ void bindShaderAttr(AppCtx *c) {
 // HELPER FUNCTIONS declarations
 void loadAttributes(AppCtx * c);
 void modelViewBoundaries(SVertex * verArr, int sizeArr, GLfloat * rxmin, GLfloat * rxmax, GLfloat  * rymin, GLfloat * rymax, GLfloat  * rzmin, GLfloat * rzmax);
-
+GLfloat diameter(SVertex * verArr, int sizeArr, GLfloat xcenter, GLfloat ycenter, GLfloat zcenter);
 // functions IMPLEMENTATION
 
 void renderFrame(AppCtx * c) {
@@ -89,11 +95,10 @@ void viewValuesSetUp(AppCtx *c) {
     float aspect = (GLfloat) c->width / c->height;
 //    LOGI("aspect %f",aspect);
 
-    float ZNear = 1.0f;
-    float ZFar = 100.0f;
     ESMatrix perspective;
     esMatrixLoadIdentity(&perspective);
-    esPerspective(&perspective, 45.0f, aspect, ZNear, ZFar);
+    esPerspective(&perspective, ANGLE, aspect, Z_NEAR, Z_FAR);
+    
 //    LOGI("perspective");
 //    logMatrix(&perspective);
 
@@ -104,6 +109,7 @@ void viewValuesSetUp(AppCtx *c) {
 	GLfloat xcenter = (xmax + xmin) / 2;
 	GLfloat ycenter = (ymax + ymin) / 2;
 	GLfloat zcenter = (zmax + zmin) / 2;
+    GLfloat diam = diameter(c->vertices, c->numVertices, xcenter,ycenter,zcenter);
 
 	ESMatrix C; // center 
 	esMatrixLoadIdentity(&C);
@@ -113,7 +119,7 @@ void viewValuesSetUp(AppCtx *c) {
 
 	ESMatrix T; // translate
 	esMatrixLoadIdentity(&T);
-//	esTranslate(&T, -xcenter, -ycenter, ((ZNear +ZFar)/-8.0f) - zmax);
+//	esTranslate(&T, -xcenter, -ycenter, ((Z_NEAR +Z_FAR)/-8.0f) - zmax);
 	esTranslate(&T, 0, 0, -50 );
     LOGI("translate");
     logMatrix(&T);
@@ -140,9 +146,11 @@ void viewValuesSetUp(AppCtx *c) {
 //    LOGI("modelView");
 //    logMatrix(&modelView);
 
-	esTranslate(&modelView, 0, 0, -20);
+	esTranslate(&modelView, 0, 0, -FRUS_COEF * Z_FAR );
     esRotate( &modelView, 30, 1.0, 0.0, 1.0 );
-	esScale(&modelView, 3.0f, 3.0f, 3.0f);
+    float scale = 0.5f *FRUS_COEF *TANGENS * Z_FAR * diam;
+    LOGI("diameter: %f scale: %f",diam, scale);
+	esScale(&modelView, scale, scale, scale);
 	esTranslate(&modelView, -xcenter, -ycenter, -zcenter);
 
 
@@ -152,9 +160,9 @@ void viewValuesSetUp(AppCtx *c) {
 
 //    LogArrayGLui("indeces", c->faces[0],c->parts_sizes[0]);
 
-    LogVertices(c);
-    LOGI("vertices after applying mvpMatrix");
-    LOGm(&c->mvpMatrix, c);
+//    LogVertices(c);
+//    LOGI("vertices after applying mvpMatrix");
+//    LOGm(&c->mvpMatrix, c);
 
 //    glEnable(GL_CULL_FACE);
 //    checkGlError("glEnable(GL_CULL_FACE)");
@@ -225,4 +233,20 @@ void modelViewBoundaries(SVertex * verArr, int sizeArr, GLfloat * rxmin, GLfloat
 	*rymin = ymin;
 	*rzmax = zmax;
 	*rzmin = zmin;
+}
+
+GLfloat diameter(SVertex * v, int sizeArr, float xcenter, GLfloat ycenter, GLfloat zcenter) {
+    GLfloat diameter = 0;
+    GLfloat aktd, x,y,z;
+	for (int i = 1; i < sizeArr; ++i) {
+        x = v[i].x - xcenter; 
+        y = v[i].y- ycenter; 
+        z = v[i].z- zcenter; 
+        aktd = (x * x) + (y * y) + (z * z);  
+        if(aktd > diameter) {
+            diameter = aktd; 
+        }
+    }
+    diameter = sqrt(diameter);
+    return diameter;
 }
