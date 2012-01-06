2624eaee2df8b9c7df0105a25911e23cf93df871
ondrej platek <ondrej dot platek at seznam dot cz>
2012-01-06 13:17:26 +0100

    TODO modify shader according shader from example. Compile it and laod values from cpp

--- trunk/jni/shaders.h
+++ trunk/jni/shaders.h
@@ -8,17 +8,57 @@ static const char gVertexShader[] =
     "uniform mat4 c_Perspective;                \n" // TODO should be CONSTANT perspective matrix
     "attribute vec4 a_position;					\n"
     "attribute vec4 a_color;					\n"
-    "varying vec4   v_color;					\n"
+    "varying vec4   interpColor;					\n"
+    "vec3 normal = vec3(1.0,1.0,1.0); \n" // TODO load it from
     "void main() {						        \n"
-	"  v_color = a_color;				        \n"
+	"   interpColor = a_color;				        \n"
     // important is order we can think of 1.action centering_res = u_C * a_position, 2.action rotating_res = u_R * centering_res,..
- 	"  gl_Position = c_Perspective * u_P * u_S * u_R * u_C * a_position;  \n" 
+    "   mat4 modelToCameraMatrix = u_P * u_S * u_R * u_C
+ 	"   gl_Position = c_Perspective * (modelToCameraMatrix * a_position);  \n" 
+    "   vec3 normCamSpace = normalize(modelToCameraMatrix * normal);   \n"
+
     "}								            \n";
 
+// vertex shader from /opengl-tutorial/Tut 09 Lights on/data/DirVertexLighting_PCN.vert
+/*
+#version 330
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 diffuseColor;
+layout(location = 2) in vec3 normal;
+
+//smooth out vec4 interpColor;
+out vec4 interpColor;
+
+uniform vec3 dirToLight;
+uniform vec4 lightIntensity;
+
+uniform mat4 modelToCameraMatrix;
+uniform mat3 normalModelToCameraMatrix;
+
+layout(std140) uniform Projection
+{
+	mat4 cameraToClipMatrix;
+};
+
+void main()
+{
+	gl_Position = cameraToClipMatrix * (modelToCameraMatrix * vec4(position, 1.0));
+
+	vec3 normCamSpace = normalize(normalModelToCameraMatrix * normal);
+	
+	float cosAngIncidence = dot(normCamSpace, dirToLight);
+	cosAngIncidence = clamp(cosAngIncidence, 0, 1);
+	
+	interpColor = lightIntensity * diffuseColor * cosAngIncidence;
+}
+*/
+
+// our fragment shader match  /opengl-tutorial/Tut 09 Lights on/data/ColorPassthrough.frag
 static const char gFragmentShader[] = 
     "precision mediump float;		            \n"
-	"varying vec4 v_color;			            \n"
+	"varying vec4 interpColor;  	            \n"
     "void main() {					            \n"
-    "  gl_FragColor = v_color;                  \n"
+    "  gl_FragColor = interpColor;              \n"
     "}                                          \n";
 
